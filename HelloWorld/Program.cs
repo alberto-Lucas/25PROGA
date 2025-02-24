56816c50493325b36370688afcec6270f3e7c975
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-02-24 19:08:10 -0300

    PrimeiroApp aula dia 17/02/2025

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+
+        static void Main(string[] qualquercoisa)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            if (qualquercoisa.Length > 0)
+            {
+                if (qualquercoisa[0] == "1")
+                    Application.Run(new Form1());
+                else if (qualquercoisa[0] == "2")
+                    Application.Run(new Form2());
+                else
+                    Application.Run(new Form3());
+            }
+            else
+            {
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
