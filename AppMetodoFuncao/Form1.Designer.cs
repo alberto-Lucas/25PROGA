60d0f2ca1c11db3c688f6d0f60129fc3a2fece51
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-03-10 22:19:22 -0300

    AppMetodoFuncao aula dia 10/03/2025

---
+++ AppMetodoFuncao/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace AppMetodoFuncao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbbModelo = new System.Windows.Forms.ComboBox();
+            this.btnIF = new System.Windows.Forms.Button();
+            this.btnSWITCH = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnMetodo = new System.Windows.Forms.Button();
+            this.btnFuncao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe um modelo de carro:";
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(12, 25);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(121, 20);
+            this.txtModelo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Selecione um modelo de carro:";
+            // 
+            // cbbModelo
+            // 
+            this.cbbModelo.FormattingEnabled = true;
+            this.cbbModelo.Items.AddRange(new object[] {
+            "SKYLINE",
+            "UNO(COM ESCADA)",
+            "SUPRA",
+            "MONZA"});
+            this.cbbModelo.Location = new System.Drawing.Point(12, 82);
+            this.cbbModelo.Name = "cbbModelo";
+            this.cbbModelo.Size = new System.Drawing.Size(121, 21);
+            this.cbbModelo.TabIndex = 3;
+            // 
+            // btnIF
+            // 
+            this.btnIF.Location = new System.Drawing.Point(139, 23);
+            this.btnIF.Name = "btnIF";
+            this.btnIF.Size = new System.Drawing.Size(75, 23);
+            this.btnIF.TabIndex = 4;
+            this.btnIF.Text = "IF";
+            this.btnIF.UseVisualStyleBackColor = true;
+            this.btnIF.Click += new System.EventHandler(this.btnIF_Click);
+            // 
+            // btnSWITCH
+            // 
+            this.btnSWITCH.Location = new System.Drawing.Point(139, 80);
+            this.btnSWITCH.Name = "btnSWITCH";
+            this.btnSWITCH.Size = new System.Drawing.Size(75, 23);
+            this.btnSWITCH.TabIndex = 5;
+            this.btnSWITCH.Text = "SWITCH";
+            this.btnSWITCH.UseVisualStyleBackColor = true;
+            this.btnSWITCH.Click += new System.EventHandler(this.btnSWITCH_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Informe o seu Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(287, 26);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 7;
+            // 
+            // btnMetodo
+            // 
+            this.btnMetodo.Location = new System.Drawing.Point(287, 52);
+            this.btnMetodo.Name = "btnMetodo";
+            this.btnMetodo.Size = new System.Drawing.Size(75, 23);
+            this.btnMetodo.TabIndex = 8;
+            this.btnMetodo.Text = "METODO";
+            this.btnMetodo.UseVisualStyleBackColor = true;
+            this.btnMetodo.Click += new System.EventHandler(this.btnMetodo_Click);
+            // 
+            // btnFuncao
+            // 
+            this.btnFuncao.Location = new System.Drawing.Point(287, 82);
+            this.btnFuncao.Name = "btnFuncao";
+            this.btnFuncao.Size = new System.Drawing.Size(75, 23);
+            this.btnFuncao.TabIndex = 9;
+            this.btnFuncao.Text = "FUNCAO";
+            this.btnFuncao.UseVisualStyleBackColor = true;
+            this.btnFuncao.Click += new System.EventHandler(this.btnFuncao_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(459, 121);
+            this.Controls.Add(this.btnFuncao);
+            this.Controls.Add(this.btnMetodo);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSWITCH);
+            this.Controls.Add(this.btnIF);
+            this.Controls.Add(this.cbbModelo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbbModelo;
+        private System.Windows.Forms.Button btnIF;
+        private System.Windows.Forms.Button btnSWITCH;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnMetodo;
+        private System.Windows.Forms.Button btnFuncao;
+    }
+}
+
---
+++ AppMetodoFuncao/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppMetodoFuncao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIF_Click(object sender, EventArgs e)
+        {
+            if (txtModelo.Text == "F80")
+                MessageBox.Show("Vc escolheu uma Ferrari");
+            else if (txtModelo.Text == "FUSCA")
+                MessageBox.Show("Vc escolheu um VolksWagen");
+            else if (txtModelo.Text == "CIVIC")
+                MessageBox.Show("Vc escolheu um Honda");
+            else if (txtModelo.Text == "RENEGADE")
+                MessageBox.Show("Vc escolheu um JEEP");
+            else
+                MessageBox.Show("Opção não encontrada");
+        }
+
+        private void btnSWITCH_Click(object sender, EventArgs e)
+        {
+            //O USO do SWITCH definimos os CASE
+            //q são as opções fixas
+            //cada CASE precisa de um break,
+            //para encerar o SWITCH
+
+            switch(cbbModelo.SelectedIndex)
+            {
+                case 0: MessageBox.Show("Marca NISSAN");
+                    break;
+                case 1: MessageBox.Show("Marca FIAT");
+                    break;
+                case 2: MessageBox.Show("Marca TOYOTA");
+                    break;
+                case 3: MessageBox.Show("Marca CHEVROLET");
+                    break;
+                default: MessageBox.Show("Opção não encontrada");
+                    break;
+            }
+        }
+
+        private void btnMetodo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtNome.Text);
+            MessageBox.Show(txtNome.Text, "Informação!");
+            MessageBox.Show(txtNome.Text, 
+                           "Informação",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Information);
+            MsgInformacao(txtNome.Text);
+        }
+
+        //Vamos um metodo para 
+        //exibir um MessageBox de aviso
+        //pré configurado
+
+        //Para criar o método
+        //Preciso informa o tipo e o nome
+        //Void = sem retorno
+        void MsgInformacao(string Texto)
+        {
+            MessageBox.Show(Texto,  //Conteudo a ser exibido
+                "Informação!",      //Titulo da mensagem
+                MessageBoxButtons.OK, //Configuração de botoes
+                MessageBoxIcon.Information); //Configuração Icone
+        }
+
+        private void btnFuncao_Click(object sender, EventArgs e)
+        {
+            MsgInformacao("O Nome informado é: " + txtNome.Text);
+            MessageBox.Show("O Nome informado é: " + txtNome.Text);
+            MsgInformacao(MsgNome(txtNome.Text));
+            MessageBox.Show(MsgNome(txtNome.Text));
+        }
+
+        //Funcao
+        //Que retorna uma mensagem amigavel junto do Nome
+        //string RetMsgNomeAmigavel() //Exemplo
+        string MsgNome(string Nome)
+        {
+            //usamos o return
+            //para retornar o valor do metodo
+            return "O Nome informado é: " + Nome;
+        }
+    }
+}
