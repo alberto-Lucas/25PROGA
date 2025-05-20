c9d1d0254db097ade414b0bbc32daf9b9b7243f2
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-05-19 22:12:36 -0300

    AppCadArquivo aula dia 19/05/2025

---
+++ AppCadArquivo/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AppCadArquivo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Iremos salvar o cadastro
+        //em um arquivo .txt
+        //Cada cadastro será um arquivo
+        //individual
+        //Iremos aplicar os conceitos
+        //do projeto AppManipulaArquivo
+
+        //Função que validar o arquivo
+        bool ArquivoExiste(string caminho)
+        {
+            //Iremos validar a existencia do aquivo
+            //lembrando que o nome do arquivo séra o 
+            //CPF, portanto se o arquivo ja existir
+            //significa que ja possui um cadastro
+            //com aquele CPF
+            
+            return File.Exists(caminho);
+        }
+
+        //Método para gravar o conteudo no arquivo
+        void GravarArquivo(
+            string caminho, string conteudo)
+        {
+            //Iremos para a rotina de gravação do arquivo
+            //Precisamos de uma atenção no inicio
+            //Caso for o primeiro cadastro a pasta
+            //do cadastro não deve existir
+            //para isso precisamos validar se a pasta 
+            //existe, se não existir, iremos cria-la
+
+            //OBS: Não iremos usar o Try aqui
+            //pois será usado na rotina anterior
+
+            //Extrair a pasta do parametro caminho
+            //onde o arquivo será salvo
+            string pasta = 
+                Path.GetDirectoryName(caminho);
+
+            //Validar a existencia da pasta
+            //e cria-la se nescessario
+            if(!Directory.Exists(pasta))
+            {
+                Directory.CreateDirectory(pasta);
+            }
+
+            //Agora podemos gravar o conteudo
+            //no arquivo
+            File.WriteAllText(caminho, conteudo);
+        }
+
+        //Função que retorna o caminho completo
+        string GetDirArquivo(
+            string nomePasta, string nomeArquivo)
+        {
+            //O caminho será montado:
+            //Diretorio RAIZ do executavel
+            //Nome da Pasta: será o tipo do cadastro
+            //Nome do arquivo: será o CPF do cliente
+            //Ex:
+            //C:/Programas/AppCadArquivo/Clientes/00000000000.txt
+
+            //Extrair o local de instalação do exceutavel
+            string raizExe =
+                AppDomain.CurrentDomain.BaseDirectory;
+
+            //Juntar e criar o caminho completo
+            //do arquivo
+            return
+                Path.Combine(
+                    raizExe, nomePasta, nomeArquivo + ".txt");
+        }
+
+        //Função que retorna o cadastro
+        //em forma de conteudo para o arquivo
+        string GetCadastro()
+        {
+            //Iremos concatenas os dados do cadastro
+            //para gerar o conteudo a ser gravado
+            //no arquivo
+
+            string cadastro =
+                "CPF: " + txtCPF.Text +
+                Environment.NewLine +
+                "NOME: " + txtNome.Text +
+                Environment.NewLine +
+                "DATA_NASCIMENTO: " + dtpDtNascimento.Text +
+                Environment.NewLine +
+                "RG: " + txtRG.Text;
+
+            return cadastro;
+        }
+
+        //Método para salvar o cadastro
+        void Salvar()
+        {
+            //Recuperar o caminho completo
+            //Onde iremos definir o tipoCadastro: Clientes
+            //e o nome do arquivo, que será o CPF
+            //Iremos definir o nome da pasta
+            //de acordo com o tipo do cadastro
+            //nesse caso o Cadastro de Clientes
+            string caminhoCompleto =
+                GetDirArquivo("Clientes", txtCPF.Text);
+
+            //Precisamo validar a existencia do arquivo
+            //validando junto se ja existe um cadastro
+            //com o mesmo CPF informado
+            if(ArquivoExiste(caminhoCompleto))
+            {
+                MessageBox.Show(
+                    "Já existe um cadastro com este CPF.",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return; //Abortar a rotina
+            }
+
+            //Agora iremos tentar gravar o cadastro
+            //no arquivo
+            try
+            {
+                //Iremos gravar o conteudo do arquivo
+                GravarArquivo(caminhoCompleto, GetCadastro());
+
+                //Notificar o usuario
+                //que o cadatro deu certo
+
+                //OBS: Iremos apresentar o diretorio apenas
+                //para teste interno
+                //No programa final, o usuario não deve saber
+                //onde o arquivo foi salvo
+
+                MessageBox.Show(
+                    "Registro salvo com sucesso!" +
+                    Environment.NewLine +
+                    Environment.NewLine +
+                    "Salvo em: " + caminhoCompleto,
+                    "Informação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Falha ao salvar o cadastro." +
+                    Environment.NewLine +
+                    "Erro original: " + ex.Message,
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Salvar();
+        }
+    }
+}
---
+++ AppCadArquivo/frmListaClientes.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppCadArquivo
+{
+    public partial class frmListaClientes : Form
+    {
+        public frmListaClientes()
+        {
+            InitializeComponent();
+        }
+
+        //Função que retorna o caminho da pasta
+        string GetCaminhoCadastro(string nomeCadastro)
+        {
+            //nomeCadastro = Nome da Pasta
+            //Recuperamos o diretorio raiz do executavel
+            string raizExe = 
+                AppDomain.CurrentDomain.BaseDirectory;
+
+            return
+                Path.Combine(raizExe, nomeCadastro);
+        }
+
+        //Método para lista os arquivos
+        void ListarArquivos(string caminho)
+        {
+            //Iremos recuperar todos os arquivos .txt
+            //do caminho
+            //Cada arquivo terá o seu diretorio
+            //e onde cada diretorio será uma string
+            //ou seja teremos um conjunto de strings
+            //no caso sera um array de string
+
+            //Iremos tentar carregar os arquivos
+            try
+            {
+                //Recuperar os arquivos de um diretorio
+                //ja adicionando ao array
+
+                string[] arquivos =
+                    Directory.GetFiles(caminho, "*.txt");
+
+                //Validar se existe algum arquivos
+                if(arquivos.Length == 0)
+                {
+                    MessageBox.Show(
+                        "Nenhum cadastro encontrado.",
+                        "Informação",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                    return; //aborta a rotina
+                }
+
+                //Se chegou até aqui
+                //foi encontrado um ou mais arquivos
+                //Iremos popular a listBox
+                //pela listBox possuir um array de string
+                //eu consigo adicionar o nosso diretamente
+                lsbCadastros.Items.AddRange(arquivos);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Falha ao carregar os cadastros." +
+                    Environment.NewLine +
+                    "Erro original: " + ex.Message,
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCarregar_Click(object sender, EventArgs e)
+        {
+            //Iremos chamar o método para listar
+            //os arquivos
+            //Lembrando que o tipo do
+            //cadastro (nome da pasta)
+            //deve possuir o mesmo do utilizado
+            //na tela de cadastro
+            //no momento da gravação do arquivo
+            ListarArquivos(GetCaminhoCadastro("Clientes"));
+        }
+
+        private void lsbCadastros_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Primeiro precisa validar
+            //se realmente temos um registro selecionado
+            if (lsbCadastros.SelectedItem != null)
+            {
+                string caminhoArquivo = 
+                    lsbCadastros.SelectedItem.ToString();
+
+                //Podemos popular o txtConteudo com o arquivo
+                txtConteudo.Text =
+                    CarregarArquivo(caminhoArquivo);
+            }
+        }
+
+        //Função para carregar o arquivo individualmente
+        string CarregarArquivo(string arquivo)
+        {
+            //Variavel, que recebera o
+            //conteudo do arquivo
+            //que sera iniciada vazia
+            string conteudo = "";
+
+            //tentar carregar o arquivo
+            try
+            {
+                //Carregar o conteudo
+                conteudo = 
+                    File.ReadAllText(arquivo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Falha ao carregar o cadastro." +
+                    Environment.NewLine +
+                    "Erro original: " + ex.Message,
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+            //Se teve algum problema e a variavel
+            //nao foi populada, retorna vazio
+            //Se deu tudo certo, retorna o
+            //conteudo do arquivo
+
+            return conteudo;
+        }
+    }
+}
